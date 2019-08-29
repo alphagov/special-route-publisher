89df880dc57e910b0e7a2fef2c6810af30ceee81
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2019-08-29 09:27:59 +0100

    Reorganise tests
    To make them easier to understand and modify.

--- spec/lib/special_route_publisher_spec.rb
+++ spec/lib/special_route_publisher_spec.rb
@@ -2,56 +2,49 @@ require_relative '../../lib/special_route_publisher'
 require 'securerandom'
 
 RSpec.describe SpecialRoutePublisher, '#publish_special_routes' do
-  let(:publishing_api_endpoint) do
-    Plek.find('publishing-api')
+  let(:publishing_api_endpoint) { Plek.find("publishing-api") }
+
+  let(:logger) { double("logger") }
+  before { allow(Logger).to receive(:new).and_return(logger) }
+
+  let(:api_content_route) do
+    {
+      content_id: '16ca5ac1-7df5-4137-9f83-0270fcfc8bb5',
+      base_path: '/api/content',
+      title: 'Content API',
+      description: 'API exposing all content on GOV.UK.',
+      type: 'prefix',
+      publishing_app: 'special-route-publisher',
+      rendering_app: 'content-store'
+    }
   end
 
-  let(:logger) do
-    double('logger')
-  end
-
-  let(:api_content_entry) do
-    [
-      {
-        content_id: '16ca5ac1-7df5-4137-9f83-0270fcfc8bb5',
-        base_path: '/api/content',
-        title: 'Content API',
-        description: 'API exposing all content on GOV.UK.',
-        type: 'prefix',
-        publishing_app: 'special-route-publisher',
-        rendering_app: 'content-store'
-      }
-    ]
-  end
-
-  let(:invalid_entry) do
-    [
-      {
-        content_id: SecureRandom.uuid,
-        base_path: '/something-that-doesnt-matter'
-      }
-    ]
+  let(:invalid_route) do
+    {
+      content_id: SecureRandom.uuid,
+      base_path: '/something-that-doesnt-matter'
+    }
   end
 
   before do
-    allow(Logger).to receive(:new).and_return(logger)
+    allow(SpecialRoutePublisher).to receive(:load_special_routes).and_return(routes)
   end
 
-  context 'with a valid entry in special_routes.yaml' do
-    before do
-      allow(SpecialRoutePublisher).to receive(:load_special_routes).and_return(api_content_entry)
-    end
+  context "with a valid route" do
+    let(:routes) { [api_content_route] }
 
     it 'calls the Publishing API to reserve a path, put content and publish it' do
-      stub_put_path = stub_request(:put, "#{publishing_api_endpoint}/paths#{api_content_entry[0].fetch(:base_path)}").
-      with(body: "{\"publishing_app\":\"special-route-publisher\",\"override_existing\":true}")
-      stub_put_content = stub_request(:put, "#{publishing_api_endpoint}/v2/content/#{api_content_entry[0].fetch(:content_id)}")
-      stub_publish_content = stub_request(:post, "#{publishing_api_endpoint}/v2/content/#{api_content_entry[0].fetch(:content_id)}/publish").
-      with(body: "{\"update_type\":null}")
+      stub_put_path = stub_request(:put, "#{publishing_api_endpoint}/paths#{api_content_route.fetch(:base_path)}")
+        .with(body: "{\"publishing_app\":\"special-route-publisher\",\"override_existing\":true}")
+
+      stub_put_content = stub_request(:put, "#{publishing_api_endpoint}/v2/content/#{api_content_route.fetch(:content_id)}")
+
+      stub_publish_content = stub_request(:post, "#{publishing_api_endpoint}/v2/content/#{api_content_route.fetch(:content_id)}/publish")
+        .with(body: "{\"update_type\":null}")
 
       expect(logger).to receive(:info).with(/Publishing/)
 
-      SpecialRoutePublisher.publish_special_routes
+      described_class.publish_special_routes
 
       expect(stub_put_path).to have_been_requested
       expect(stub_put_content).to have_been_requested
@@ -59,15 +52,13 @@ RSpec.describe SpecialRoutePublisher, '#publish_special_routes' do
     end
   end
 
-  context 'with an invalid entry in special_routes.yaml' do
-    before do
-      allow(SpecialRoutePublisher).to receive(:load_special_routes).and_return(invalid_entry)
-    end
+  context "with an invalid route" do
+    let(:routes) { [invalid_route] }
 
     it 'logs an error message and carries on' do
       expect(logger).to receive(:error).with(/Unable/)
 
-      SpecialRoutePublisher.publish_special_routes
+      described_class.publish_special_routes
     end
   end
 end
