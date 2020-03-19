fae1b2677fe2c697436d62d0c2226e0c3f949e20
Simon Hughesdon <simon.hughesdon@digital.cabinet-office.gov.uk>
2020-03-19 11:51:17 +0000

    Add task to publish coronavirus routes
    We want a minor update task too, because we don't necessarily want to
    send emails if we're just updating the description that appears in search.
    
    At present this doesn't affect anything on the page itself, this is for
    publishing the route and appearing in search results (hence the "answer"
    document type).

--- lib/special_route_publisher.rb
+++ lib/special_route_publisher.rb
@@ -4,8 +4,16 @@ require "yaml"
 
 class SpecialRoutePublisher
   def self.publish_special_routes
-    special_routes = load_special_routes
-    new.publish_routes(special_routes)
+    new.publish_routes(load_special_routes)
+  end
+
+  def self.publish_coronavirus_routes
+    new.publish_routes(load_coronavirus_routes)
+  end
+
+  def self.minor_update_coronavirus_routes
+    minor_update_routes = load_coronavirus_routes.map { |route| route.merge(update_type: "minor") }
+    new.publish_routes(minor_update_routes)
   end
 
   def publish_routes(routes)
@@ -31,8 +39,8 @@ class SpecialRoutePublisher
         publishing_api.put_content(
           route.fetch(:content_id),
           base_path: route.fetch(:base_path),
-          document_type: "special_route",
-          schema_name: "special_route",
+          document_type: route.fetch(:document_type, "special_route"),
+          schema_name: route.fetch(:document_type, "special_route"),
           title: route.fetch(:title),
           description: route.fetch(:description, ""),
           locale: "en",
@@ -60,7 +68,10 @@ class SpecialRoutePublisher
   def self.load_special_routes
     YAML.load_file("./data/special_routes.yaml")
   end
-  private_class_method :load_special_routes
+
+  def self.load_coronavirus_routes
+    YAML.load_file("./data/coronavirus_routes.yaml")
+  end
 
   def logger
     @logger ||= Logger.new(STDOUT)
