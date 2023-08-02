0dcd184f3473fbf60e538424b7ad59f9ef5ead86
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2023-08-02 13:47:47 +0100

    Add task to publish all routes for a particular app
    - If we're moving frontend apps published_api tasks into this app, we should make publishing all routes for one app into a single task, so that it's as close to calling the rake task on the old app as possible.
    - Update readme to describe all the existing rake tasks under the Running locally heading (the first time in the doc the rake tasks are mentioned).
    - Refactor SpecialRoutePublisher.publish_routes method (tiny decrease in a giant method size, improved testability)

--- lib/special_route_publisher.rb
+++ lib/special_route_publisher.rb
@@ -7,6 +7,16 @@ class SpecialRoutePublisher
     new.publish_routes(load_special_routes)
   end
 
+  def self.publish_special_routes_for_app(app_name)
+    routes = load_special_routes.filter { |r| r[:rendering_app] == app_name }
+
+    if routes.any?
+      new.publish_routes(routes)
+    else
+      puts "No routes for #{app_name} in /data/special_routes.yaml"
+    end
+  end
+
   def self.publish_one_route(base_path)
     route = load_special_routes.find { |r| r[:base_path] == base_path }
 
@@ -40,52 +50,56 @@ class SpecialRoutePublisher
     new.publish_routes(load_homepage)
   end
 
+  def publish_route(route, time)
+    type = route.fetch(:type, "exact")
+
+    logger.info("Publishing #{type} route #{route.fetch(:base_path)}, routing to #{route.fetch(:rendering_app)}...")
+
+    # Always request a path reservation before publishing the special route,
+    # with the flag to override any existing publishing app.
+    # This allows for routes that were previously published by other apps to
+    # be added to `special_routes.yaml` and "just work".
+    publishing_api.put_path(
+      route.fetch(:base_path),
+      publishing_app: "special-route-publisher",
+      override_existing: true,
+    )
+
+    publishing_api.put_content(
+      route.fetch(:content_id),
+      base_path: route.fetch(:base_path),
+      document_type: route.fetch(:document_type, "special_route"),
+      schema_name: route.fetch(:document_type, "special_route"),
+      title: route.fetch(:title),
+      description: route.fetch(:description, ""),
+      locale: "en",
+      details: {},
+      routes: [
+        {
+          path: route.fetch(:base_path),
+          type:,
+        },
+      ],
+      publishing_app: "special-route-publisher",
+      rendering_app: route.fetch(:rendering_app),
+      public_updated_at: time.now.iso8601,
+      update_type: route.fetch(:update_type, "major"),
+    )
+
+    if route[:links]
+      publishing_api.patch_links(
+        route.fetch(:content_id),
+        links: route[:links],
+      )
+    end
+
+    publishing_api.publish(route.fetch(:content_id))
+  end
+
   def publish_routes(routes)
     time = (Time.respond_to?(:zone) && Time.zone) || Time
     routes.each do |route|
-      type = route.fetch(:type, "exact")
-
-      logger.info("Publishing #{type} route #{route.fetch(:base_path)}, routing to #{route.fetch(:rendering_app)}...")
-
-      # Always request a path reservation before publishing the special route,
-      # with the flag to override any existing publishing app.
-      # This allows for routes that were previously published by other apps to
-      # be added to `special_routes.yaml` and "just work".
-      publishing_api.put_path(
-        route.fetch(:base_path),
-        publishing_app: "special-route-publisher",
-        override_existing: true,
-      )
-
-      publishing_api.put_content(
-        route.fetch(:content_id),
-        base_path: route.fetch(:base_path),
-        document_type: route.fetch(:document_type, "special_route"),
-        schema_name: route.fetch(:document_type, "special_route"),
-        title: route.fetch(:title),
-        description: route.fetch(:description, ""),
-        locale: "en",
-        details: {},
-        routes: [
-          {
-            path: route.fetch(:base_path),
-            type:,
-          },
-        ],
-        publishing_app: "special-route-publisher",
-        rendering_app: route.fetch(:rendering_app),
-        public_updated_at: time.now.iso8601,
-        update_type: route.fetch(:update_type, "major"),
-      )
-
-      if route[:links]
-        publishing_api.patch_links(
-          route.fetch(:content_id),
-          links: route[:links],
-        )
-      end
-
-      publishing_api.publish(route.fetch(:content_id))
+      publish_route(route, time)
     rescue KeyError => e
       logger.error("Unable to publish #{route} due to an error: #{e}")
     end
